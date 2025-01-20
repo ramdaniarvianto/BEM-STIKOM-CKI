462f217fe06a3b9b331af7a8c3ee28b336caa299
ramdaniarvianto <ramdaniarvianto1@gmail.com>
2025-01-20 12:17:10 +0700

    sambutan

--- pages/sambutan.php
+++ pages/sambutan.php
@@ -20,24 +20,6 @@ header("Pragma: no-cache");
 <body>
     <?php include "includes/navbar.php"; ?>
 
-    <header>
-        <h1>Selamat Datang di Website <span class="h-bem">BEM STIKOM CKI</span> Kabinet <span class="h-kabinet">Dharma Satvavira Dialektika</span></h1>
-        <img src="assets/images/bem/raker.png" alt="BEM STIKOM CKI 2024/2025">
-    </header>
-
-    <main>
-        <div class="sambutan">
-            <div class="sambutan-image">
-                <img src="assets/images/ketuwakil/rajabowo.png" alt="Presiden dan Wakil Presiden Mahasiswa BEM STIKOM CKI">
-            </div>
-            <div class="sambutan-text">
-                <h2>
-                    Presiden dan Wakil Presiden <br>
-                    Mahasiswa STIKOM CKI
-                </h2>
-                <p><a href="pages/sambutan.php">Sambutan</a></p>
-            </div>
-        </div>
-    </main>
+    
 </body>
 </html>
